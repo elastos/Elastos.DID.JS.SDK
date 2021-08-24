84409abb969dc060c67a7e7ea7621da1f5ab69f6
chenyukaola <chenyu_kaola@126.com>
2021-08-24 15:38:30 +0800

    Update the copyright in every file.

--- src/comparablemap.ts
+++ src/comparablemap.ts
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2021 Elastos Foundation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 import { Collections } from "./collections";
 import type { Comparable } from "./comparable";
 
@@ -13,7 +35,7 @@ export class ComparableMap<K extends Comparable<K>, V> extends Map<K,V> {
     public delete(k: K): boolean {
         for (let e of this.entries()) {
             if (e[0].equals(k))
-                return super.delete(e[0]);     
+                return super.delete(e[0]);
         }
         return false;
     }
@@ -21,9 +43,9 @@ export class ComparableMap<K extends Comparable<K>, V> extends Map<K,V> {
     public has(k: K): boolean {
         for (let e of this.entries()) {
             if (e[0].equals(k))
-                return true;     
+                return true;
         }
-        return false;        
+        return false;
     }
 
     /**
