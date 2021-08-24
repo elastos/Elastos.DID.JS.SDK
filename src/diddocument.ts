0026616de540d2e2cace162fd008e240d72e4c6f
Jingyu <jingyu.niu@gmail.com>
2021-08-24 11:53:39 +0800

    CU-cqwzp7 - Fixed the DID and store export/import error

--- src/diddocument.ts
+++ src/diddocument.ts
@@ -1379,6 +1379,7 @@ export class DIDDocument extends DIDEntity<DIDDocument> {
         doc.controllerDocs = new ComparableMap<DID, DIDDocument>(this.controllerDocs);
         if (this.multisig != null)
             doc.multisig = DIDDocumentMultiSignature.newFromMultiSignature(this.multisig);
+        doc.effectiveController = this.effectiveController;
         doc.publicKeys = new ComparableMap<DIDURL, DIDDocumentPublicKey>(this.publicKeys);
         doc.authenticationKeys = new ComparableMap<DIDURL, DIDDocumentPublicKey>(this.authenticationKeys);
         doc.authorizationKeys = new ComparableMap<DIDURL, DIDDocumentPublicKey>(this.authorizationKeys);
@@ -1399,6 +1400,7 @@ export class DIDDocument extends DIDEntity<DIDDocument> {
 
         doc.controllers = this.controllers;
         doc.controllerDocs = this.controllerDocs;
+        doc.effectiveController = this.effectiveController;
         doc.multisig = this.multisig;
         doc.publicKeys = this.publicKeys;
         doc.authenticationKeys = this.authenticationKeys;
