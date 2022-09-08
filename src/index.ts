24f40de59eac0d4954e6b1275cf53f598c0de527
chenyukaola <chenyu_kaola@126.com>
2022-09-08 10:01:06 +0800

    Exclude libsodium from the bundle

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ import { Cipher, EncryptionStream, DecryptionStream } from "./didencryption";
     });
 } */
 
-let __VERSION__ = "2.2.11";
+let __VERSION__ = "2.2.12";
 
 if (typeof window != 'undefined') {
     if ("elastos_did_ver" in window)
