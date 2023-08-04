0f88e01fdfaa6cc93d5c4240f845e3946ab13721
chenyukaola <chenyu_kaola@126.com>
2023-08-04 17:35:47 +0800

    Update the version

--- src/index.ts
+++ src/index.ts
@@ -106,7 +106,7 @@ import { Cipher, EncryptionStream, DecryptionStream } from "./didencryption";
     });
 } */
 
-let __VERSION__ = "2.2.12";
+let __VERSION__ = "2.2.13";
 
 if (typeof window != 'undefined') {
     if ("elastos_did_ver" in window)
